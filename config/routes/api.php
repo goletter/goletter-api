4cfa2fc0f61428ce85eb9a166a3d275cb702babf
goletter <goletter@outlook.com>
2026-07-08 01:20:47 +0800

    feat:优化代码

--- config/routes/api.php
+++ config/routes/api.php
@@ -6,6 +6,6 @@ use Goletter\Server\Router\Router;
 Router::addGroup('/api', function () {
     Router::get('/index', [\App\Controller\IndexController::class, 'index']);
 
-    Router::get('/google/auth-url', [\App\Controller\GoogleController::class, 'index']);
-    Router::get('/google/callback', [\App\Controller\GoogleController::class, 'index']);
+    Router::get('/google/auth-url', [\App\Controller\GoogleController::class, 'authUrl']);
+    Router::get('/google/callback', [\App\Controller\GoogleController::class, 'callback']);
 });
