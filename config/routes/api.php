b754954f793cdc2d9d26bb4cc6e0608c71c390a0
yong <goletter@outlook.com>
2026-08-04 16:23:19 +0800

    feat:bm导出调整

--- config/routes/api.php
+++ config/routes/api.php
@@ -1,16 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use Goletter\Mtls\Middleware\ClientCertificateMiddleware;
 use Goletter\Server\Router\Router;
 
 Router::addGroup('/api', function () {
     Router::get('/index', [\App\Controller\IndexController::class, 'index']);
-
-    Router::get('/google/auth-url', [\App\Controller\GoogleController::class, 'authUrl']);
-    Router::get('/google/callback', [\App\Controller\GoogleController::class, 'callback']);
-}, ['middleware' => [ClientCertificateMiddleware::class]]);
-
-Router::addGroup('/api', function () {
-    Router::get('/test', [\App\Controller\IndexController::class, 'test']);
 });
