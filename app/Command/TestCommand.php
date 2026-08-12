62eaa574030610f7d4abbc2967308caa269e7327
goletter <goletter@outlook.com>
2026-08-12 23:39:03 +0800

    feat:优化代码

--- app/Command/TestCommand.php
+++ app/Command/TestCommand.php
@@ -43,45 +43,15 @@ class TestCommand extends HyperfCommand
         // dd($updates);
         $chatId = '-1004388791491';
         // $users = $bot->getGroupUsers($chatId);
-        $accounts = [
-            ['code' => '1907426453234759', 'name' => 'YFPL-GF-0717+7-03'],
-            ['code' => '966914535733264', 'name' => 'YFPL-EA-0720+8-01'],
-            ['code' => '937050745619959', 'name' => 'YFPL-cd-0701+6-01'],
-            ['code' => '946093961490991', 'name' => 'YFPL-cd-0701+6-02'],
-            ['code' => '1480762437134528', 'name' => 'YFPL-EA-0720+8-03'],
-            ['code' => '953232684338562', 'name' => 'YFPL-FM-0716+8-01'],
-        ];
-
-        $intro = "闲置账户提醒\n\n"
+        $message = "闲置账户提醒\n\n"
             . "为避免资源浪费，优化账户使用率，账户闲置时间大于15天，系统将自动进行回收；\n"
             . "如暂时不使用该账户，请前往账户管理页面提交回收。\n"
             . "感谢您的理解与支持！\n\n"
             . "以下为贵司【闲置3天及以上】账户清单：\n\n";
 
-        $tableHeader = sprintf("%-17s %s\n", '账户ID', '账户名称');
-        $table = $tableHeader;
-        $messages = [];
-
-        foreach ($accounts as $account) {
-            $row = sprintf("%-17s %s\n", $account['code'], $account['name']);
-
-            if (strlen($intro) + strlen($table) + strlen($row) > 3500 && $table !== $tableHeader) {
-                $messages[] = $intro . '<pre>' . htmlspecialchars($table, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . '</pre>';
-                $intro = '';
-                $table = $tableHeader;
-            }
-
-            $table .= $row;
-        }
-
-        $messages[] = $intro . '<pre>' . htmlspecialchars($table, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . '</pre>';
-
-        foreach ($messages as $message) {
-            $bot->sendMessage([
-                'chat_id' => $chatId,
-                'text' => $message,
-                'parse_mode' => 'HTML',
-            ]);
-        }
+        $bot->sendMessage([
+            'chat_id' => $chatId,
+            'text' => $message,
+        ]);
     }
 }
