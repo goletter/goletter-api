505685dab0a974ab245a59ae1551a48487c59b47
goletter <goletter@outlook.com>
2026-09-20 01:51:36 +0800

    feat:优化代码

---
+++ app/Service/Action/Account/CreateAction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @desc:
+ * @author: 你的名字
+ * @date: 2026/9/20 00:51
+ */
+
+namespace App\Service\Action\Account;
+
+class CreateAction
+{
+
+}
---
+++ app/Service/Action/Account/UpdateAction.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @desc:
+ * @author: 你的名字
+ * @date: 2026/9/20 00:51
+ */
+
+namespace App\Service\Action\Account;
+
+use Goletter\Server\Service\Service;
+
+class CreateAction extends Service
+{
+    public function handle(array $data)
+    {
+        //
+    }
+}
